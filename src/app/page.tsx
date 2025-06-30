edebcbd39927e70fb9f947b9b8e421238ff032de
Rishu Raj <rrishu561@gmail.com>
2025-06-30 14:41:05 +0530

    init: data access layer(trpc)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Homepage () {
+import { trpc, getQueryClient } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import Client from "./client"
+import { Suspense } from "react"
+
+export default async function Homepage() {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Rishu PREFETCH" }))
   return (
-    <div>
-      Hello world
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
