5ec23087b861a3e56459a5f7fca08e2be08a8ca9
Rishu Raj <rrishu561@gmail.com>
2025-07-12 13:26:11 +0530

    init : ai agent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { useTRPC } from "@/trpc/client"
 import { getQueryClient, trpc } from "@/trpc/server"
 import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
 import { toast } from "sonner"
 
 export default function Homepage() {
+  const [value, setValue] = useState("")
   const trpc = useTRPC()
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
@@ -15,7 +18,8 @@ export default function Homepage() {
   }))
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button disabled={invoke?.isPending} onClick={() => invoke.mutateAsync({ text: "Rishu" })}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button disabled={invoke?.isPending} onClick={() => invoke.mutateAsync({ value: value })}>
         Invoke BG Job
       </Button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,22 @@
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait a moment", "30s");
-    await step.sleep("wait a moment", "10s");
-    await step.sleep("wait a moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+    const codeAgent = createAgent({
+      name: "codeAgent",
+      system:
+        "You are an expert software engineer and AI assistant. You can write code, debug, and help with software development tasks.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+    console.log("codeAgent output:", output);
+
+    return { output };
   }
 );
