6a393fb02dff11db000316abac66b09b1b688858
Rishu Raj <rrishu561@gmail.com>
2025-07-13 16:55:07 +0530

    feat: e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { Agent, openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("assembyl-nextjs-test2");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "codeAgent",
       system:
@@ -15,8 +22,13 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet: ${event.data.value}`
     );
-    console.log("codeAgent output:", output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
