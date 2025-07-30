5dd0d6f39be90593aa08bd223f4c7dda353b4b11
Rishu Raj <rrishu561@gmail.com>
2025-07-30 20:13:25 +0530

    fix: prod build

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,14 +1,14 @@
-"use client"
+// "use client"
 
-import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
+// import { useTRPC } from "@/trpc/client";
+// import { useSuspenseQuery } from "@tanstack/react-query";
 
-export default function Client() {
-    const trpc = useTRPC();
-    const { data } = useSuspenseQuery(trpc.createAI.queryOptions({ text: "Rishu PREFETCH" }));
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            {JSON.stringify(data, null, 2)}
-        </div>
-    );
-}
+// export default function Client() {
+//     const trpc = useTRPC();
+//     const { data } = useSuspenseQuery(trpc.createAI.queryOptions({ text: "Rishu PREFETCH" }));
+//     return (
+//         <div className="flex flex-col items-center justify-center h-screen">
+//             {JSON.stringify(data, null, 2)}
+//         </div>
+//     );
+// }
